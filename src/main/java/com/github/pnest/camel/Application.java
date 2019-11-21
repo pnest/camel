2f3861212bbcca6a9287b45632601245def5f02d
Nesterovich, Pavel <p.nesterovich@itransition.com>
2019-11-21 16:37:13 +0300

    Add logging processor

--- src/main/java/com/github/pnest/camel/Application.java
+++ src/main/java/com/github/pnest/camel/Application.java
@@ -1,13 +1,25 @@
 package com.github.pnest.camel;
 
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
-    public static void main(String[] args) {
-        final ApplicationContext context = new ClassPathXmlApplicationContext("spring/camel-application-context.xml");
-        System.out.println(context.getApplicationName());
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        final ApplicationContext context = new ClassPathXmlApplicationContext(
+                "spring/camel-application-context.xml",
+                "spring/database-application-context.xml"
+        );
+
+        log.debug(context.getApplicationName());
+
+        Thread.sleep(TimeUnit.MINUTES.toMillis(5));
     }
 
 }
